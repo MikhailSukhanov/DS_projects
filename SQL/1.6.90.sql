80d88bff1aadb2a7ee5c0a1fa5d7870641aa2dc5
Mikhail Sukhanov <MikhailSukhanov55@yandex.ru>
2023-02-19 10:38:21 +0300

    Create 1.6.90.sql

---
+++ SQL/1.6.90.sql
@@ -0,0 +1,5 @@
+SELECT name, SUM((DATEDIFF(date_last, date_first) + 1) * per_diem) AS Сумма
+FROM trip
+GROUP BY name
+HAVING COUNT(*) > 3
+ORDER BY SUM((DATEDIFF(date_last, date_first) + 1) * per_diem) DESC
